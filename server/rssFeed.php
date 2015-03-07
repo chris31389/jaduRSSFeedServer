bbe1c110a1999de3f68aec79845f49d97ab1a1d5
Chris Butler <christopher_g_butler@hotmail.co.uk>
2015-03-07 00:52:48 +0000

    adding getFeed()

--- server/rssFeed.php
+++ server/rssFeed.php
@@ -14,8 +14,41 @@
     public $valid = false;
     public $info = array();
 
+    public function __construct(){
+
+    }
+
+    public static function withURL($url){
+      $instance = new self();
+      $instance->RssFeedByUrl($url);
+      return $instance;
+    }
+
+    public static function withJsonArray($jsonArr){
+      $instance = new self();
+      $instance->RssFeedByJsonArray($jsonArr);
+      return $instance;
+    }
+
+    private function RssFeedByJsonArray($jsonArr){
+      $arr = json_decode($jsonArr,true);
+      $this->url = $arr[0]['rssUrl'];
+      $this->errorMessage = "none";
+      $valid = true;
+      $this->info = $this->newArray($arr[0]['rssUrl'],$arr[0]['name'],$arr[0]['desc'],$arr[0]['link']);
+    }
+
+    private function newArray($url, $title, $desc, $link){
+      return array(
+          "rssUrl" => $url,
+          "name" => $title,
+          "desc" => $desc,
+          "link" => $link
+        );
+    }
+
     // This is designed to read the RSS Reed Channel Header.
-    function RssFeed($url){
+    private function RssFeedByUrl($url){
       $this->url = $url;
       $headers = @get_headers($url);
       if($headers !== false){
@@ -34,14 +67,19 @@
           $link = $channel->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
           $desc = $channel->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
 
-          $this->info = array(
-            "url" => $url,
-            "name" => $title,
-            "desc" => $desc,
-            "link" => $link
-          );
+          $this->info = $this->newArray($url, $title, $desc, $link);
         };
       }
+    }
+
+    public function getFeed(){
+      $arr = array();
+
+      // GET ALL THE FEED INFORMATION AND POPULATE INTO ARRAY.
+      $arr[] = $this->info;
+      
+
+      return json_encode($arr);
     }
 
     function GetError(){
@@ -54,10 +92,12 @@
     case "GET":
 
       // PREVENTING SQL INJECTION BY MAKING SURE id IS INTEGER
-      if(!isset($_GET['id']) || !is_int($_GET['id'])){
+      if(!isset($_GET['id']) || !is_numeric($_GET['id'])){
         $output = $db->Select();
       }else{
-        $output = $db->SelectID($_GET['id']);
+        $info = $db->SelectID($_GET['id']);
+        $rss = RssFeed::withJsonArray($info);
+        $output = $rss->getFeed();
       }
 
       break;
@@ -66,7 +106,7 @@
       $request_body =file_get_contents("php://input");
       $payload = json_decode($request_body);
       $url = urldecode($payload->url);
-      $feed = new RssFeed($url);
+      $feed = RssFeed::withURL($url);
       if($feed->valid){
         $output = $db->Insert($feed->info);
       }
